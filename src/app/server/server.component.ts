60138267ddc6064a8afb31e624d62ba0626d530e
redelization <ricardosmithco@gmail.com>
2019-10-31 08:59:44 -0500

    initial commit with textbox and submit button

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: []
+})
+export class ServerComponent {
+
+    serverId = 10;
+    serverStatus = 'online';
+
+    getServerStatus(){
+        return this.serverStatus;
+    }
+    
+}
